643461b995e4e738b4538abe02086a761a4c21e3
Yannick Daveluy <yannick.daveluy@gmail.com>
2024-09-16 20:34:09 +0200

    export copyright notice in smpcat/smppkg

--- src/language/generator/generator.ts
+++ src/language/generator/generator.ts
@@ -2,6 +2,15 @@ import type { AstNode, URI } from 'langium';
 
 export type Task = () => Promise<void>;
 export type TaskAcceptor = (task: Task) => void;
+
+let enableGeneratedByComment: boolean = true;
+
+export function setGeneratedBy(value: boolean): void {
+     enableGeneratedByComment = value;
+
+} export function isGeneratedBy(): boolean {
+     return enableGeneratedByComment;
+}
 export interface XsmpGenerator {
      generate: (node: AstNode, projectUri: URI, acceptTask: TaskAcceptor) => void
      clean: (projectUri: URI) => void
