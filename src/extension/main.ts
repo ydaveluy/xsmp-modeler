39f9555cae14f7a064fbaef204dd45039e1c4f6a
Yannick Daveluy <yannick.daveluy@gmail.com>
2025-05-15 08:56:21 +0200

    async init of language server

--- src/extension/main.ts
+++ src/extension/main.ts
@@ -9,8 +9,8 @@ import { GetServerFileContentRequest, RegisterContributor } from '../language/ls
 let client: LanguageClient;
 
 // This function is called when the extension is activated.
-export function activate(context: vscode.ExtensionContext): void {
-    client = startLanguageClient(context);
+export async function activate(context: vscode.ExtensionContext): Promise<void> {
+    client = await startLanguageClient(context);
     BuiltinLibraryFileSystemProvider.register(context);
 
     // New project Wizard
@@ -28,7 +28,7 @@ export function deactivate(): Thenable<void> | undefined {
     return client.stop();
 }
 
-function startLanguageClient(context: vscode.ExtensionContext): LanguageClient {
+async function startLanguageClient(context: vscode.ExtensionContext): Promise<LanguageClient> {
     const serverModule = context.asAbsolutePath(path.join('out', 'language', 'main.cjs')),
         // The debug options for the server
         // --inspect=6009: runs the server in Node's Inspector mode so VS Code can attach to the server for debugging.
@@ -63,7 +63,7 @@ function startLanguageClient(context: vscode.ExtensionContext): LanguageClient {
             clientOptions
         );
     // Start the client. This will also launch the server
-    client.start();
+    await client.start();
     return client;
 }
 
